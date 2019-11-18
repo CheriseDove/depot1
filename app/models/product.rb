65932a9adab3618fbcea484dbe3d36d9db25aa3e
CheriseDove <r.coleman7@nuigalway.ie>
2019-11-18 12:37:03 +0000

    added who_bought and atom.feeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   has_many :line_items
+  has_many :orders, through: :line_items
   validates :image_url, allow_blank: true, format: {
   with: %r{\.(gif|jpg|png)\Z}i,
   message: 'must be a URL for GIF, JPG or PNG image.'
